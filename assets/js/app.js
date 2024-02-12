cbb9659196b6d48b92adf0c2efdfe7047a577ab4
amaliehausmann <amaliehausmann@gmail.com>
2024-02-12 13:25:44 +0100

    get product data finished #11

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,11 +8,24 @@ function GetCategoryData(callback) {
 
     .then(data => {
         console.log('fetched category data:', data);
+        callback(data);
     })
 }
 
-function ReceiveCategoryData(data) {
-    console.log('Processing data:', data);
+GetCategoryData(ReceiveCategoryData);
+
+function GetProductData(callback) {
+    const productAPIURL = 'https://dummyjson.com/products'
+
+    fetch(productAPIURL)
+    .then(productResponse => {
+        return productResponse.json();
+    })
+
+    .then(data => {
+        console.log('fetched product data:', data);
+        callback(data);
+    })
 }
 
-GetCategoryData(ReceiveCategoryData);
+GetProductData(ReceiveProductData);
