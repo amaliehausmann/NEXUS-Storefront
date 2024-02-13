37990aa341eb171df58c0479aa4420defc684156
amaliehausmann <amaliehausmann@gmail.com>
2024-02-13 14:38:34 +0100

    #12

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,10 @@ function GetCategoryData(callback) {
 
     fetch(categoryAPIURL)
     .then(CategoryResponse => {
+        if(!CategoryResponse.ok){
+            throw new Error ('failed to fetch data. Status: ' + CategoryResponse.status);
+        }
+
         return CategoryResponse.json();
     })
 
@@ -10,15 +14,27 @@ function GetCategoryData(callback) {
         console.log('fetched category data:', data);
         callback(data);
     })
+
+    .catch (error => {
+        console.error('Error fetching category data:', error.message);
+    })
+}
+
+function ReceiveCategoryData(data) {
+    let myCategories = data
 }
 
 GetCategoryData(ReceiveCategoryData);
 
 function GetProductData(callback) {
-    const productAPIURL = 'https://dummyjson.com/products'
+    const productAPIURL = 'https://dummyjson.com/products?limit=0'
 
     fetch(productAPIURL)
     .then(productResponse => {
+        if(!productResponse.ok){
+            throw new Error ('failed to fetch data. Status: ' + productResponse.status);
+        }
+
         return productResponse.json();
     })  
 
@@ -26,6 +42,17 @@ function GetProductData(callback) {
         console.log('fetched product data:', data);
         callback(data);
     })
+
+    .catch (error => {
+        console.error('Error fetching category data:', error.message);
+    })
 }
+
+function ReceiveProductData(data){
+    let MyProducts = data.products
+    let MyFeaturedProducts = [MyProducts[Math.floor(Math.random() * 101)], MyProducts[Math.floor(Math.random() * 101)], MyProducts[Math.floor(Math.random() * 101)]]
+    console.log(MyFeaturedProducts);
+}
+
 
 GetProductData(ReceiveProductData);
